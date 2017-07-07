ce93f07d36d146d99e605e9ec7ec351c5395e562
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-07-07 10:34:02 +0530

    Update codeforces.py

--- codeforces.py
+++ codeforces.py
@@ -1,18 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
-import re
+def codeforces(username):
+    head = 'http://codeforces.com/profile/'
+    var = username
+    URL = head + var
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content,'html.parser')
 
-head = "http://codeforces.com/profile/"
-var = str(input())
-URL = head + var
-
-page  = requests.get(URL)
-soup = BeautifulSoup(page.content,'html.parser')
-
-a = list(soup.findAll('li'))
-print(a)
-for i in a:
-    if 'Contest rating:' in i:
-        print(i)
-        
-
+    listRating = list(soup.findAll('div',class_="user-rank"))
+    CheckRating = listRating[0].get_text()  #Check for rated or unrated
+    if str(CheckRating) == '\nUnrated \n':
+        # print('Not rated')
+        out = 1000000
+        return(out)
+    else:
+    # print('rated')
+        listinfo = list((soup.find('div',class_="info")).findAll('li'))
+        string = (listinfo[0].get_text())
+        string = string.replace(" ","")
+        str1,str2 = string.split('(')   # Well,.. don't judge me
+        str3,str4 = str1.split(':') 
+        out = int((str4.strip()))
+        print(out)
+        return(out)
